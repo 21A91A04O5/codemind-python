9d7fc233a2415de3bb3f4c6386494f873a621255
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2022-11-01 15:37:33 +0530

    n=int(input()) x=list(map(int,input().split())) sum=0 for i in x:     if i%2!=0:         sum=sum+i print(sum)

--- ODD_Sum.py
+++ ODD_Sum.py
@@ -1,7 +1,7 @@
 n=int(input())
 x=list(map(int,input().split()))
 sum=0
-for i in range(len(x)):
-    if x[i]%2!=0:
-        sum=sum+x[i]
+for i in x:
+    if i%2!=0:
+        sum=sum+i
 print(sum)        
